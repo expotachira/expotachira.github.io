a5794569a5106be5d7476972798d69c08a036b1c
amilcarpw <amilcar@parawebs.com>
2014-10-03 14:59:02 -0430

    send post

--- js/expofiss.js
+++ js/expofiss.js
@@ -5,8 +5,7 @@ $(function() {
 		$("#bodytop").stop().scrollTo( 0,500);
 	});
 
-	$.getJSON( "json/tipoSegmento.json", function( data ) {
-		console.log(data);
+	$.getJSON( "http://192.168.1.12/expofiss/index.php/tipoempresa", function( data ) {
 		var select=$(".infobody .selectpicker");
 		$.each(data, function(index, val) {
 			var option=document.createElement('option');
@@ -17,8 +16,33 @@ $(function() {
 		});
 		$('.selectpicker').selectpicker();
 	});
-});
 
+	stipo = GetURLParameter('stype')|0;
+	sid = GetURLParameter('sid')|0;
+
+	function GetURLParameter(sParam)
+	{
+	    var sPageURL = window.location.search.substring(1);
+	    var sURLVariables = sPageURL.split('&');
+	    for (var i = 0; i < sURLVariables.length; i++)
+	    {
+	        var sParameterName = sURLVariables[i].split('=');
+	        if (sParameterName[0] == sParam)
+	        {
+	        	return sParameterName[1];
+	        }
+	 	}
+	}
+
+	$(document).on("click",".infobody .btn",function(evt){
+		$.post( "http://192.168.1.12/expofiss/index.php/savenew",($(infoform).serialize()+'&pre_ruta='+stipo+'&pre_rutaid='+sid), function( data ) {
+			console.warn("----------Resultado----------");
+			console.log(data);
+		},"json").fail(function(res){
+			console.log("se cago");
+		});
+	});
+});
 // 'pre_emp' => $vars['empresa'],
 // 'pre_con' => $vars['contacto'],
 // 'pre_tel' => $vars['telefono'],
