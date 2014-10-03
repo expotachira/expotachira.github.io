2c787548e43516c863df98e9b4fd66e84b6c7c7f
amilcarpw <amilcar@parawebs.com>
2014-10-03 14:06:22 -0430

    Se carga el tipo de segmento en el formulario.

--- js/expofiss.js
+++ js/expofiss.js
@@ -5,14 +5,25 @@ $(function() {
 		$("#bodytop").stop().scrollTo( 0,500);
 	});
 
-	$('ul.menu li').click(function(e){
-		e.preventDefault();
-		var elemento=this;
-		$('ul.menu li').removeClass('active');
-		$(elemento).addClass('active');
+	$.getJSON( "json/tipoSegmento.json", function( data ) {
+		console.log(data);
+		var select=$(".infobody .selectpicker");
+		$.each(data, function(index, val) {
+			var option=document.createElement('option');
+			var text=document.createTextNode(val.tipo_des);
+			option.appendChild(text);
+			option.setAttribute('value',val.tipo_id);
+			select.append(option);
+		});
+		$('.selectpicker').selectpicker();
 	});
-
 });
 
-
+// 'pre_emp' => $vars['empresa'],
+// 'pre_con' => $vars['contacto'],
+// 'pre_tel' => $vars['telefono'],
+// 'pre_ema' => $vars['email'],
+// 'pre_tip' => $vars['tipo'],
+// 'pre_ruta' => $vars['ruta'],
+// 'pre_rutaid' => $vars['rutaid'],
 
